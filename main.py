ba56ad0d2fcdfe6e5040090abeb24d1de6bae4d6
Makjur-Ai <kevin.seso@gmail.com>
2025-08-08 04:23:12 +0200

    function to get text

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text():
+    with open("/home/kevin/workspace/github.com/makjur-ai/bookbot/books/frankenstein.txt") as f: 
+        text = f.read()
+    return text
+
+def main():
+    print(get_book_text())
+
+main()
