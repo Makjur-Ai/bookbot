d01465965253b990d6c2f673e6bb1c8f16bf061b
Makjur-Ai <kevin.seso@gmail.com>
2025-08-08 05:13:37 +0200

    modified to print count of words

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def get_book_text():
     with open("/home/kevin/workspace/github.com/makjur-ai/bookbot/books/frankenstein.txt") as f: 
         text = f.read()
-    return text
+        num_words = len(text.split(None, -1))
+    return f"{num_words} words found in the document."
 
 def main():
     print(get_book_text())
