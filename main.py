c5b0f9d84d67ae9ac9a9e55f7e330197d0b7ea06
Makjur-Ai <kevin.seso@gmail.com>
2025-08-10 04:45:07 +0200

    count charaacters

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ def get_book_text():
     return text
 
 from stats import get_num_words
-
+from stats import get_num_type_letters
 def main():
     book_text = get_book_text()
+    letter_dict = get_num_type_letters(book_text)
     print(get_num_words(book_text))
+    print(letter_dict)
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def get_num_words(text):
     num_words = len(text.split(None, -1))
     return f"{num_words} words found in the document."
+
+def get_num_type_letters(text):
+  letter = text.lower()
+  character = {}
+  for char in letter:
+     if char not in character:
+        character[char] = 1
+     else:
+        character[char] += 1
+  return character
+  
