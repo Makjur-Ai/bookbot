bad83d6fe83c28ed804f2400b36e8cbdaef87b86
Makjur-Ai <kevin.seso@gmail.com>
2025-08-12 05:53:06 +0200

    print statements in main

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 from stats import get_num_words
 from stats import get_num_type_letters
+from stats import sort_dictionary
 
-def get_book_text(path="books/frankenstein.txt"):
+def header_book_count(path, book_text):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {get_num_words(book_text)} total words")
+
+def character_count(letter_dict):
+    print("--------- Character Count -------")
+    sorted_char_list = sort_dictionary(letter_dict)
+    for item_dict in sorted_char_list:
+        char = item_dict["char"]
+        num = item_dict["num"]
+        print(f"{char}: {num}")
+
+
+
+def main():
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    letter_dict = get_num_type_letters(book_text)
+    sorted_letters = sort_dictionary(letter_dict)
+    header_book_count(book_path, book_text)
+    character_count(letter_dict)
+      
+
+def get_book_text(path):
     with open(path) as f: 
         text = f.read()
     return text
-
-def main():
-    book_text = get_book_text()
-    letter_dict = get_num_type_letters(book_text)
-    print(get_num_words(book_text))
-    print(letter_dict)
-
 
 main()
--- stats.py
+++ stats.py
@@ -1,14 +1,26 @@
 def get_num_words(text):
     num_words = len(text.split(None, -1))
-    return f"{num_words} words found in the document."
+    print(num_words)
 
 def get_num_type_letters(text):
-  letter = text.lower()
-  character = {}
-  for char in letter:
-     if char not in character:
-        character[char] = 1
-     else:
-        character[char] += 1
-  return character
-  
+    letter = text.lower()
+    characters = {}
+    for char in letter:
+        if char not in characters:
+            characters[char] = 1
+        else:
+            characters[char] += 1
+    return characters
+
+def sort_key(char):
+    return char["num"]
+
+def sort_dictionary(characters):
+    sorted_chars = []
+    for char, num in characters.items():
+        if char.isalpha():
+            sorted_chars.append({"char": char, "num": num})
+    
+    sorted_chars.sort(reverse=True, key=sort_key)
+
+    return sorted_chars
